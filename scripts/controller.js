1d2d4fc8e45d679fb89ac82239917689f9943656
Vasily Crespo <vasilycrespo@gmail.com>
2016-05-16 13:24:18 -0400

    added an assignee filter

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,20 +1,39 @@
 var app = angular.module('myExporter', []);
 
 app.controller("exportCtrl", function($scope, $http, $timeout){
+	var filter = "";
 
+	$scope.done = false;
+	$scope.assignee = "all";
 	$scope.user;
 	$scope.repo;
 
 	$scope.export = function () {
-
-
-		console.log($scope.repo);
+		$scope.done = false;
+		switch($scope.assignee) {
+		    case "all":
+		        break;
+		    case "unassigned":
+		    	filter = '?assignee=none';
+		        break;
+		    case "somebody":
+		    	filter = '?assignee=*';
+		        break;
+		    case "to":
+		    	if(!$scope.assignedUser) {
+		    		alert("Write and assigneed user to filter with");
+		    		return;
+		    	}
+		    	filter = '?assignee='+$scope.assignedUser;
+		        break;
+		}
 		if ($scope.repo && $scope.user) {
 			$scope.load = true;
 			$http({
 			  method: 'GET',
-			  url: 'https://api.github.com/repos/' + $scope.user + '/' + $scope.repo + '/issues'
+			  url: 'https://api.github.com/repos/' + $scope.user + '/' + $scope.repo + '/issues' + filter,
 			}).then(function successCallback(response) {
+				$scope.done = true;
 				$scope.load = false;
 				console.log(response.data);
 				$scope.output = response.data;
